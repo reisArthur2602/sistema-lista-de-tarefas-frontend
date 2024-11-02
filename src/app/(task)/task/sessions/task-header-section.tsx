4d1f58c23f5987234ecddded55a1343b82650f43
@dev.arthur <contato.arthurreis222@gmail.com>
2024-11-02 14:52:04 -0300

    style: insert responsive design to taskHeaderSection component

--- src/app/(task)/task/sessions/task-header-section.tsx
+++ src/app/(task)/task/sessions/task-header-section.tsx
@@ -3,7 +3,7 @@ import { Plus } from "lucide-react";
 
 const TaskHeaderSection = () => {
   return (
-    <section className="mb-8 flex justify-between">
+    <section className="mb-8 flex flex-col justify-between gap-4 sm:flex-row">
       <div className="flex flex-col">
         <h1 className="text-xl font-bold">Minhas Tarefas</h1>
         <p className="text-sm text-zinc-400">Gerenciar minhas tarefas</p>
