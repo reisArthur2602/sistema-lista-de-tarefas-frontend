92cc0d0f860a9912b6717c979c55f5a72ffded70
@dev.arthur <contato.arthurreis222@gmail.com>
2024-11-03 19:40:39 -0300

    feat: insert token when making request

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { cookies } from "next/headers";
 
 const apiConnection = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_KEY,
@@ -7,5 +8,15 @@ const apiConnection = axios.create({
 apiConnection.interceptors.response.use(null, (error) => {
   return Promise.reject(error);
 });
+
+apiConnection.interceptors.request.use((config) => {
+  const token = cookies().get("session-access-token")?.value as string;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+}, null);
 
 export { apiConnection };
